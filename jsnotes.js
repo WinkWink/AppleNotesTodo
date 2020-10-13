23ff4023d10441ec2911d380b9b68c3711ec1a15
amanda winkler <amanda.winkler44@gmail.com>
2020-10-13 14:49:36 -0400

    tweaky

--- jsnotes.js
+++ jsnotes.js
@@ -1,5 +1,5 @@
-var startDate = new Date("2020-09-23"); 
-var endDate = new Date("2020-12-31");
+var startDate = new Date("2020-10-13"); 
+var endDate = new Date("2020-11-31");
 
 
 var getDateArray = function(start, end){
@@ -26,12 +26,13 @@ var dateArr = getDateArray(startDate,endDate);
 document.write("<h1>To Do List</h1>");
 for (var i = 0; i < dateArr.length; i++) {
 	document.write("<h3 id='split'>" + dateArr[i] + "</h3>");
-	document.write("<p>Morning Ritual</p>");
+	document.write("<p>Check in With Body</p>");
 	document.write("<p>Tech Ladies</p>");
 	document.write("<p>Gratitude Journal</p>");
 	document.write("<p>Cat Box</p>");
 	document.write("<p>Github</p>");
-	document.write("<p>Water Plants</p>");
+	document.write("<p>Read cayare</p>");
 	document.write("<p>Journal</p>");
+
 }
 
