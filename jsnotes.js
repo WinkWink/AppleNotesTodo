ef72b753ecac085fb2a990c08766e35fa5bb958c
amanda winkler <amanda.winkler44@gmail.com>
2021-05-25 19:15:49 -0400

    june and sunday edit

--- jsnotes.js
+++ jsnotes.js
@@ -1,5 +1,5 @@
-var startDate = new Date("2021-5-01"); 
-var endDate = new Date("2021-5-31");
+var startDate = new Date("2021-6-01"); 
+var endDate = new Date("2021-6-31");
 
 
 var getDateArray = function(start, end){
@@ -23,17 +23,26 @@ var dateArr = getDateArray(startDate,endDate);
 
 //output 
 
+
 document.write("<h1>To Do List April</h1>");
 for (var i = 0; i < dateArr.length; i++) {
 	document.write("<h3 id='split'>" + dateArr[i] + "</h3>");
-	document.write("Stretch<br>");
+	document.write("Smoothie + Coffee + Water<br>");
+	document.write("Take Meds<br>");
+	document.write("Noom<br>");
+	document.write("The Daily<br>");
 	document.write("Tech Ladies Board<br>");
 	document.write("Gratitude Journal<br>");
 	document.write("Cat Box<br>");
-	document.write("Take Meds<br>");
-	document.write("Read come as you are<br>");
-	document.write("Breathe<br>");
-	document.write("Work on book club<br>");
+	document.write("Tonk Box<br>");
+	document.write("React Practice<br>");
+	document.write("Exercise @ 530<br>");
+
+
+	var trying = dateArr[i].toString();
+	if (trying.includes("Sun")) {
+	 document.write("Watch Ziwe<br>");
+	}
 
 }
 
