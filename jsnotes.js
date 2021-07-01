2fb1fe71a8c19cf96ef92b712b4a5b2f0698f2dd
amanda winkler <amanda.winkler44@gmail.com>
2021-07-01 10:00:55 -0400

    July and days of the week split

--- jsnotes.js
+++ jsnotes.js
@@ -1,5 +1,5 @@
-var startDate = new Date("2021-6-01"); 
-var endDate = new Date("2021-6-31");
+var startDate = new Date("2021-7-01"); 
+var endDate = new Date("2021-7-30");
 
 
 var getDateArray = function(start, end){
@@ -24,7 +24,7 @@ var dateArr = getDateArray(startDate,endDate);
 //output 
 
 
-document.write("<h1>To Do List April</h1>");
+document.write("<h1>To Do List June</h1>");
 for (var i = 0; i < dateArr.length; i++) {
 	document.write("<h3 id='split'>" + dateArr[i] + "</h3>");
 	document.write("Smoothie + Coffee + Water<br>");
@@ -35,14 +35,24 @@ for (var i = 0; i < dateArr.length; i++) {
 	document.write("Gratitude Journal<br>");
 	document.write("Cat Box<br>");
 	document.write("Tonk Box<br>");
-	document.write("React Practice<br>");
-	document.write("Exercise @ 530<br>");
-	document.write("dash<br>");
+	document.write("Code<br>");
+	document.write("Exercise @ 6<br>");
 
 
 	var trying = dateArr[i].toString();
-	if (trying.includes("Sun")) {
-	 document.write("Watch Ziwe<br>");
+	if (trying.includes("Mon")){
+	}else if(trying.includes("Tue")){
+		document.write("tech ladies career chat");
+	}else if(trying.includes("Wed")){
+		document.write("apply to jobs<br>");
+	}else if(trying.includes("Thur")){
+		document.write("apply to jobs<br>");
+	}else if(trying.includes("Fri")){
+		document.write("dash<br>");
+	}else if(trying.includes("Sat")){
+		document.write("dash<br>");
+	}else if(trying.includes("Sun")){
+		document.write("dash<br>");
 	}
 
 }
